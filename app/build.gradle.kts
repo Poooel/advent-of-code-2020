e2b6b89c9dcb0ed12ce3245afea50c162e6d9136
Paul Queruel <paul.queruel@protonmail.com>
2020-11-30 19:24:19 +0000

    Updated http library, script path and gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("com.github.ajalt.clikt:clikt:3.0.1")
     implementation("com.github.ajalt.mordant:mordant:2.0.0-alpha1")
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.3")
-    implementation("khttp:khttp:1.0.0")
+    implementation("com.github.kittinunf.fuel:fuel:2.3.0")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
